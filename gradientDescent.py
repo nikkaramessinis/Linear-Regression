d0f6dac796836d7537b5deae5687b0cadf10a365
nikkaramessinis <nikaramessinis@gmail.com>
2020-07-18 21:15:46 +0100

    Images

--- gradientDescent.py
+++ gradientDescent.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.lines as mlines
 
-points = [(0.4, 0.7),(0.3, 0.9)]
+points = [(0.4, 1),(0.2, 0.9)]
 
 def F(w):
     return sum((w* x - y)**2 for x, y in points)
